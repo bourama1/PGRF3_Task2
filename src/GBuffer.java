540bd3059f8765267498dbe3a8324d61ff8525cd
bourama1 <bourama1@uhk.cz>
2022-11-27 12:37:31 +0100

    Created GBuffer

---
+++ src/GBuffer.java
@@ -0,0 +1,86 @@
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+
+import static Utils.Const.HEIGHT;
+import static Utils.Const.WIDTH;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.glDrawBuffers;
+import static org.lwjgl.opengl.GL30.*;
+
+public class GBuffer {
+
+    private static final int TOTAL_TEXTURES = 4;
+    private final int gBufferId;
+    private final int[] textureIds;
+
+    public GBuffer() {
+        // Create G-Buffer
+        gBufferId = glGenFramebuffers();
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, gBufferId);
+
+        textureIds = new int[TOTAL_TEXTURES];
+        glGenTextures(textureIds);
+
+        // Create textures for GBuffer
+        for (int i = 0; i < TOTAL_TEXTURES; i++) {
+            glBindTexture(GL_TEXTURE_2D, textureIds[i]);
+            int attachmentType;
+            // Depth texture
+            if (i == TOTAL_TEXTURES - 1) {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT32F, WIDTH, HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT,
+                        (ByteBuffer) null);
+                attachmentType = GL_DEPTH_ATTACHMENT;
+            } else {
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, WIDTH, HEIGHT, 0, GL_RGB, GL_FLOAT, (ByteBuffer) null);
+                attachmentType = GL_COLOR_ATTACHMENT0 + i;
+            }
+
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+            glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+            // Attach texture to the G-Buffer
+            glFramebufferTexture2D(GL_FRAMEBUFFER, attachmentType, GL_TEXTURE_2D, textureIds[i], 0);
+        }
+
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            IntBuffer intBuff = stack.mallocInt(TOTAL_TEXTURES);
+            int[] values = {GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2, GL_COLOR_ATTACHMENT3, GL_COLOR_ATTACHMENT4, GL_COLOR_ATTACHMENT5};
+            for (int value : values) {
+                intBuff.put(value);
+            }
+            intBuff.flip();
+            glDrawBuffers(intBuff);
+        }
+
+        // Unbind
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
+    public int getGBufferId() {
+        return gBufferId;
+    }
+
+    public int[] getTextureIds() {
+        return textureIds;
+    }
+
+    public int getPositionTexture() {
+        return textureIds[0];
+    }
+
+    public int getDepthTexture() {
+        return textureIds[TOTAL_TEXTURES-1];
+    }
+
+    public void cleanUp() {
+        glDeleteFramebuffers(gBufferId);
+
+        if (textureIds != null) {
+            for (int i=0; i<TOTAL_TEXTURES; i++) {
+                glDeleteTextures(textureIds[i]);
+            }
+        }
+    }
+}
---
+++ src/Utils/Const.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public final class Const {
+    public static final float CAM_SPEED = 0.1f;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    private Const() {
+    }
+}
